eb3b16a2d384360acd0fc423eb31ec09efc39591
anjalidalal <anjalimaria2002@gmail.com>
2022-09-10 23:22:55 +0530

    Two player game

--- index.js
+++ index.js
@@ -1,21 +1,39 @@
 //console.log(Math.random());
-
+const main = document.querySelector(".main");
 const boxes = document.querySelectorAll(".goal");
-const ball = document.querySelector(".ball");
-const red = document.getElementById("red");
-const blue = document.getElementById("blue");
+const boxContainer = document.querySelector(".box");
 
-boxes.forEach((box) => {
+const array = [
+  [0, 1, 2, 3, 4, 5, 6],
+  [7, 8, 9, 10, 11, 12],
+  [13, 14, 15, 16, 17, 18],
+  [19, 20, 21, 22, 23, 24],
+  [25, 26, 27, 28, 29, 30],
+  [31, 32, 33, 35, 36, 37],
+];
+
+boxes.forEach((box, i) => {
   box.addEventListener("click", (e) => {
-    console.log(e, "hello", e.target);
+    console.log(e, e.target);
+
+    const ball = document.createElement("div");
+    ball.classList.add("ball");
+    main.prepend(ball, boxContainer);
 
     const { x, y } = e;
 
-    ball.classList.add("move-ball");
+    // ball.style.top = `${y - e.clientTop}px`;
+    // ball.style.left = `${x - e.layerX}px`;
+    // ball.style.transform = `translateY(${486}px)`;
+
     ball.style.top = `${y - e.clientTop}px`;
     ball.style.left = `${x - e.layerX}px`;
     ball.style.transform = `translateY(${
       e.target.offsetTop + e.target.clientHeight
     }px)`;
+
+    for (let i = 0; i < boxes.length; i++) {
+      console.log(boxes[i], boxes.length);
+    }
   });
 });
